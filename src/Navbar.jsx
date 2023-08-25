de061d25b878b01d66831bbefa5e66b64d5604b4
Rishiraj17 <rajrishi17021709@gmail.com>
2023-08-26 00:39:42 +0530

    navbar resposnsive fixed

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import kiit from "./images/KIIT_LOGO_1.png";
 import ksac from "./images/KSAC_LOGO_1.png";
-import back from "./images/Background_Image.png";
+// import back from "./images/Background_Image.png";
+import kritarthlogo from './images/kritarth logo 1 1.png'
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
-
 function Navbar() {
   const [click, setclick] = useState(false);
   const [about, setabout] = useState(false);
@@ -15,7 +15,7 @@ function Navbar() {
   const [reg, setreg] = useState(false);
   const [navColor, setnavColor] = useState("transparent");
   const listenScrollEvent = () => {
-    window.scrollY > 30 ? setnavColor("slate-500/40") : setnavColor("transparent");
+    window.scrollY != 0 ? setnavColor("slate-400") : setnavColor("transparent");
 
   };
   useEffect(() => {
@@ -26,10 +26,12 @@ function Navbar() {
   }, []);
   return (
     <>
-      <header className={`text-gray-90 bg-${navColor} hidden w-screen md:flex shadow-2xl z-50 shadow-slate-700/10 body-font  md:sticky top-0 `}>
+      <header className={`text-gray-90 bg-${navColor} hidden w-screen md:flex shadow-2xl z-50 shadow-slate-700/10 body-font  lg:sticky top-0 `}>
         <div className="container mx-auto flex flex-wrap px-5 py-1 flex-col md:flex-row items-center">
           <div className="flex title-font font-base items-center text-gray-900 mb-4 md:mb-0">
-            <img src={kiit} alt="kiitlogo" className="h-10 md:h-full" />
+            <a href="/">
+              <img src={kritarthlogo} alt="kritarthlogo" className="h-10 md:h-[60px] logo_shadow mix-blend-multiply" />
+            </a>
           </div>
           <nav className="md:ml-auto md:mr-auto flex  flex-wrap text-white items-center md:text-lg text-sm space-x-2 justify-center nav_font">
             <Link
@@ -70,17 +72,18 @@ function Navbar() {
           </nav>
           <div className="inline-flex items-center  ">
             <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 ">
-              <img src={ksac} alt="kiitlogo" className="h-10 md:h-[85px]" />
+              <img src={ksac} alt="ksaclogo" className="h-10 md:h-[60px] p-1" />
+              <img src={kiit} alt="kiitlogo" className="h-10 md:h-[60px] p-1" />
             </a>
           </div>
         </div>
       </header>
 
       <nav className="bg-slate-500/40 md:hidden sticky top-0 z-50">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-7xl px-2 md:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
             <div
-              className="absolute inset-y-0 left-0 flex items-center sm:hidden"
+              className="absolute inset-y-0 left-0 flex items-center md:hidden"
               onClick={() => setclick(!click)}
             >
               {/* Mobile menu button*/}
@@ -127,16 +130,19 @@ function Navbar() {
                 )}
               </button>
             </div>
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
               <div className="flex flex-shrink-0 items-center">
-                <img
-                  className="h-8 w-auto"
-                  src={kiit}
-                  alt="Your Company"
-                />
+                <a href="/">
+
+                  <img
+                    className="h-8 w-auto"
+                    src={kritarthlogo}
+                    alt="Your Company"
+                  />
+                </a>
               </div>
             </div>
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
               {/* Profile dropdown */}
               <div className="relative ml-3">
                 <div>
@@ -150,9 +156,14 @@ function Navbar() {
                     <span className="absolute -inset-1.5" />
                     <span className="sr-only">Open user menu</span>
                     <img
-                      className="h-8 w-8 "
-                      src={ksac}
+                      className="h-10 w-12 py-[2px] p-1"
+                      src={kiit}
                       alt=""
+                    />
+                    <img
+                      className="h-10 w-auto p-1"
+                      src={ksac}
+                      alt="Your Company"
                     />
                   </button>
                 </div>
@@ -162,10 +173,11 @@ function Navbar() {
         </div>
         {/* Mobile menu, show/hide based on menu state. */}
         {click && (
-          <div className="sm:hidden fixed backdrop-blur-sm bg-slate-500/40 w-screen z-50" id="mobile-menu">
+          <div className="md:hidden fixed backdrop-blur-sm bg-slate-500/40 w-screen z-50" id="mobile-menu">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {/* Current: "bg-gray-900 text-white", Default: "text-white hover:bg-gray-700 hover:text-white" */}
               <Link
+                onClick={() => setclick(false)}
                 to={'/about'}
                 className=" text-white hover:bg-green-600 block rounded-md px-3 py-2 text-base font-medium"
                 aria-current="page"
@@ -173,24 +185,28 @@ function Navbar() {
                 About
               </Link>
               <Link
+                onClick={() => setclick(false)}
                 to={'/event'}
                 className="text-white hover:bg-green-600 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
               >
                 Events
               </Link>
               <Link
+                onClick={() => setclick(false)}
                 to={'/leads'}
                 className="text-white hover:bg-green-600 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
               >
                 Leads
               </Link>
               <Link
+                onClick={() => setclick(false)}
                 to={'/form'}
                 className="text-white hover:bg-green-600 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
               >
                 Register
               </Link>
               <Link
+                onClick={() => setclick(false)}
                 to={'/contacts'}
                 className="text-white hover:bg-green-600 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
               >
